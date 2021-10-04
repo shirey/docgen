d558d5e01e7b755a391361d46a1713eaedde468d
Bill Shirey <shirey@pitt.edu>
2021-10-03 21:42:26 -0400

    print directory listings

--- code-gen/info.py
+++ code-gen/info.py
@@ -1,8 +1,16 @@
 import os
 
+def printdir(dir):
+    print("---" + dir)
+    print(os.listdir(dir))
 print(os.getcwd())
-for k, v in sorted(os.environ.items()):
-    print(k+':', v)
-print('\n')
-# list elements in path environment variable
-[print(item) for item in os.environ['PATH'].split(';')]
+printdir(os.getcwd())
+printdir(".")
+printdir("../")
+printdir("../../")
+
+#for k, v in sorted(os.environ.items()):
+#    print(k+':', v)
+#print('\n')
+## list elements in path environment variable
+#[print(item) for item in os.environ['PATH'].split(';')]
